ab63f7c608297ede79aaf889f87306f6be231ae6
Ishrat Badami <99205142+ishrat-tl@users.noreply.github.com>
2023-08-18 14:05:28 -0400

    Update setup.py
    change the name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='aot',
+    name='DeAOT',
     version='0.1.0',
     description='A semi-supervised video object segmentation approach.',
     url='https://github.com/ishrat-tl/aot-benchmark',
