5c58bc8c7de805af5033c5a51b4bfba1b9d5629d
Ishrat Badami <99205142+ishrat-tl@users.noreply.github.com>
2023-08-18 21:49:05 -0400

    Update fpn.py

--- aot/networks/decoders/fpn.py
+++ aot/networks/decoders/fpn.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from networks.layers.basic import ConvGN
+from layers.basic import ConvGN
 
 
 class FPNSegmentationHead(nn.Module):
